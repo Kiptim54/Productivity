24434d1a1c88202fd736cbf040993e21b905684b
Brenda Kiptim <kiptim54@gmail.com>
2020-08-01 22:46:45 +0300

    only added animation to the background transition

---
+++ src/components/Todo.test.js
@@ -0,0 +1,10 @@
+// import React, { Component } from 'react';
+
+// const TestTodos = () => {
+//     const div = 
+//     return (  );
+// }
+ 
+// export default TestTodos;
+
+// clearly still struggling to test a react component
