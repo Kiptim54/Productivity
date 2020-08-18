9c096442d2bc76c8897d7f3d3845db428f3d0eab
Brenda Kiptim <kiptim54@gmail.com>
2020-08-18 17:01:26 +0300

    cleared cache

---
+++ build/precache-manifest.fd5bc2a30c9e5de8efd08b93cabbcbb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c2ae5d4572258031107b912e070335a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24abcd487a19c72459f6",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b28a3e35e03cde7e8def",
+    "url": "/static/css/main.dd0073d3.chunk.css"
+  },
+  {
+    "revision": "24abcd487a19c72459f6",
+    "url": "/static/js/2.08db40a4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.08db40a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b28a3e35e03cde7e8def",
+    "url": "/static/js/main.75e68b94.chunk.js"
+  },
+  {
+    "revision": "baa2b955c5eeaf5fc216",
+    "url": "/static/js/runtime-main.2f035890.js"
+  }
+]);
